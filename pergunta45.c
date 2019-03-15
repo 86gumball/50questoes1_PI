6a24af932cbbd14eb9a76294335b53652d582ff7
86gumball <lucasruben2000@gmail.com>
2019-03-15 11:36:11 +0000

    pergunta45.c (correto)

---
+++ pergunta45.c
@@ -0,0 +1,14 @@
+int unionMSet (int N, int v1[N], int v2[N], int r[N]){
+	int i;
+	int res = 0;
+	for (i = 0; i < N; i++){
+		if (v1[i] > v2[i]){
+			r[i] = v1[i];
+			res += v1[i];
+		} else {
+			r[i] = v2[i];
+			res += v2[i];
+		}
+	}
+	return res;
+}
